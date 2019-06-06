98bbb1567ee7cfc54b9e5e444e928f934e5ade4f
Yana <yanahasson@gmail.com>
2019-06-06 16:18:04 +0200

    Fix minor atlasbranch error

--- mano_train/networks/branches/atlasbranch.py
+++ mano_train/networks/branches/atlasbranch.py
@@ -1,15 +1,10 @@
-import argparse
-from enum import Enum
-
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as torch_f
 import trimesh
 
-from handobjectdatasets.queries import TransQueries, BaseQueries
+from handobjectdatasets.queries import TransQueries
 
 from mano_train.networks.branches import atlasutils
 from mano_train.networks.branches.laplacianloss import LaplacianLoss
@@ -52,21 +47,27 @@ class AtlasBranch(nn.Module):
         self.predict_trans = predict_trans
         if self.predict_trans:
             self.decode_trans = torch.nn.Sequential(
-                torch.nn.Linear(self.bottleneck_size, int(self.bottleneck_size / 2)),
+                torch.nn.Linear(
+                    self.bottleneck_size, int(self.bottleneck_size / 2)
+                ),
                 torch.nn.ReLU(),
                 torch.nn.Linear(int(self.bottleneck_size / 2), 3),
             )
         self.predict_scale = predict_scale
         if self.predict_scale:
             self.decode_scale = torch.nn.Sequential(
-                torch.nn.Linear(self.bottleneck_size, int(self.bottleneck_size / 2)),
+                torch.nn.Linear(
+                    self.bottleneck_size, int(self.bottleneck_size / 2)
+                ),
                 torch.nn.ReLU(),
                 torch.nn.Linear(int(self.bottleneck_size / 2), 1),
             )
             self.decode_scale[-1].bias.data.fill_(1)
 
         if mode == "sphere":
-            test_mesh = trimesh.creation.icosphere(subdivisions=inference_ico_divisions)
+            test_mesh = trimesh.creation.icosphere(
+                subdivisions=inference_ico_divisions
+            )
 
             # Initialize inference vertices and faces
             test_faces = np.array(test_mesh.faces)
@@ -75,7 +76,9 @@ class AtlasBranch(nn.Module):
             test_verts, test_faces = atlasutils.create_disk(14)
         else:
             raise ValueError("{} not in [sphere|disk]".format(mode))
-        self.test_verts = torch.Tensor(np.array(test_verts).astype(np.float32)).cuda()
+        self.test_verts = torch.Tensor(
+            np.array(test_verts).astype(np.float32)
+        ).cuda()
         self.test_faces = test_faces
 
     def forward(self, img_features):
@@ -83,14 +86,18 @@ class AtlasBranch(nn.Module):
         if self.predict_trans:
             translations = self.decode_trans(img_features)
         # Sample random points on unit sphere
-        rand_grid = img_features.new_empty((img_features.size(0), 3, self.points_nb))
+        rand_grid = img_features.new_empty(
+            (img_features.size(0), 3, self.points_nb)
+        )
         rand_grid.data.normal_(0, 1)
         rand_grid = rand_grid / torch.sqrt(
             torch.sum(rand_grid ** 2, dim=1, keepdim=True)
         )
 
         # Add grid to image features
-        img_features = img_features.unsqueeze(2).repeat(1, 1, rand_grid.size(2))
+        img_features = img_features.unsqueeze(2).repeat(
+            1, 1, rand_grid.size(2)
+        )
         dec_img_features = torch.cat((rand_grid, img_features), 1)
 
         # Decoder pass
@@ -123,7 +130,9 @@ class AtlasBranch(nn.Module):
                 1, 1, test_grid.size(2)
             )
         else:
-            dec_img_features = img_features.unsqueeze(2).repeat(1, 1, test_grid.size(2))
+            dec_img_features = img_features.unsqueeze(2).repeat(
+                1, 1, test_grid.size(2)
+            )
 
         dec_img_features_grid = torch.cat((test_grid, dec_img_features), 1)
         verts = self.decoder(dec_img_features_grid).transpose(2, 1)
@@ -151,16 +160,15 @@ def edge_loss(edges, faces):
     edges_A = edges[:, faces[:, 0]]
     edges_B = edges[:, faces[:, 1]]
     edges_C = edges[:, faces[:, 2]]
-    # edge_lengths_A = torch.norm(edges_B - edges_A, dim=2)
-    # edge_lengths_B = torch.norm(edges_C - edges_B, dim=2)
-    # edge_lengths_C = torch.norm(edges_A - edges_C, dim=2)
-    # all_edges = torch.cat([edge_lengths_C, edge_lengths_B, edge_lengths_A], dim=1)
-    # edge_loss = torch_f.mse_loss(all_edges, all_edges.mean(1, keepdim=True).repeat(1, all_edges.shape[1]))
     edge_lengths_A = torch.sum((edges_B - edges_A) ** 2, dim=2)
     edge_lengths_B = torch.sum((edges_C - edges_B) ** 2, dim=2)
     edge_lengths_C = torch.sum((edges_A - edges_C) ** 2, dim=2)
-    all_edges = torch.cat([edge_lengths_C, edge_lengths_B, edge_lengths_A], dim=1)
-    mean_edge_size = all_edges.mean(1, keepdim=True).repeat(1, all_edges.shape[1])
+    all_edges = torch.cat(
+        [edge_lengths_C, edge_lengths_B, edge_lengths_A], dim=1
+    )
+    mean_edge_size = all_edges.mean(1, keepdim=True).repeat(
+        1, all_edges.shape[1]
+    )
     edge_loss = torch.mean(torch.abs(all_edges - mean_edge_size))
     return edge_loss
 
@@ -185,7 +193,9 @@ class AtlasLoss:
         self.edge_regul_lambda = edge_regul_lambda
         self.lambda_laplacian = lambda_laplacian
         if lambda_laplacian:
-            self.laplacian_loss = LaplacianLoss(laplacian_faces, laplacian_verts)
+            self.laplacian_loss = LaplacianLoss(
+                laplacian_faces, laplacian_verts
+            )
         self.atlas_loss = atlas_loss
         if self.atlas_loss == "chamfer":
             self.chamfer_loss = atlasutils.ChamferLoss()
@@ -205,17 +215,17 @@ class AtlasLoss:
                 and ("objpointscentered3d" in preds)
             ):
                 obj_centroids = target[TransQueries.objpoints3d].mean(1)
-                # obj_centroids = (target[TransQueries.objpoints3d].max(
-                #     1)[0] + target[TransQueries.objpoints3d].min(1)[0]) / 2
-                trans3d_loss = torch_f.mse_loss(preds["objtrans"], obj_centroids)
+                trans3d_loss = torch_f.mse_loss(
+                    preds["objtrans"], obj_centroids
+                )
                 atlas_losses["atlas_trans3d"] = trans3d_loss
                 centered_objpoints3d = target[
                     TransQueries.objpoints3d
                 ] - obj_centroids.unsqueeze(1)
                 if "objscale" in preds:
-                    # obj_scales = torch.norm(centered_objpoints3d, 2,
-                    #                         2).max(1)[0] / 100
-                    obj_scales = torch.norm(centered_objpoints3d, 2, 2).max(1)[0]
+                    obj_scales = torch.norm(centered_objpoints3d, 2, 2).max(1)[
+                        0
+                    ]
                     scale3d_loss = torch_f.mse_loss(
                         preds["objscale"], obj_scales.unsqueeze(1)
                     )
@@ -229,7 +239,6 @@ class AtlasLoss:
                         preds["objpointscentered3d"], centered_objpoints3d
                     )
                     sym_loss = torch.mean(loss_1 + loss_2)
-                    atlas_losses["atlas_chamfer"] = chamfer_loss
                 obj_mesh = preds["objpointscentered3d"]
 
                 # Chamfer loss with incorporated trans
@@ -238,7 +247,9 @@ class AtlasLoss:
                         preds["objpoints3d"], target[TransQueries.objpoints3d]
                     )
                     sym_final_loss = torch.mean(final_loss_1 + final_loss_2)
-                atlas_losses["final_{}_loss".format(self.atlas_loss)] = sym_final_loss
+                atlas_losses[
+                    "final_{}_loss".format(self.atlas_loss)
+                ] = sym_final_loss
                 final_loss = (
                     self.lambda_atlas * sym_loss
                     + self.final_lambda_atlas * sym_final_loss
@@ -252,20 +263,27 @@ class AtlasLoss:
                 if "objpoints3d" in preds and self.lambda_atlas:
                     if self.atlas_loss == "chamfer":
                         loss_1, loss_2 = self.chamfer_loss(
-                            preds["objpoints3d"], target[TransQueries.objpoints3d]
+                            preds["objpoints3d"],
+                            target[TransQueries.objpoints3d],
                         )
                         sym_loss = torch.mean((loss_1 + loss_2))
                     final_loss = self.lambda_atlas * sym_loss
                     obj_mesh = preds["objpoints3d"]
             # Regularization losses
-            if self.edge_regul_lambda is not None and (self.edge_regul_lambda > 0):
+            if self.edge_regul_lambda is not None and (
+                self.edge_regul_lambda > 0
+            ):
                 edge_regul_loss = edge_loss(obj_mesh, preds["objfaces"])
                 atlas_losses["atlas_edge_regul"] = edge_regul_loss
-                final_loss = final_loss + self.edge_regul_lambda * edge_regul_loss
+                final_loss = (
+                    final_loss + self.edge_regul_lambda * edge_regul_loss
+                )
             if self.lambda_laplacian:
                 laplacian_loss = self.laplacian_loss(obj_mesh)
                 atlas_losses["atlas_laplac"] = laplacian_loss
-                final_loss = final_loss + self.lambda_laplacian * laplacian_loss
+                final_loss = (
+                    final_loss + self.lambda_laplacian * laplacian_loss
+                )
         else:
             sym_loss = None
             final_loss = torch.Tensor([0]).cuda()
